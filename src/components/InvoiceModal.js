1168b305d111293bbc12820d2e28abf011428025
krishdevopsmoweb <krish.devops.moweb@gmail.com>
2025-08-18 18:15:53 +0530

    change file

--- src/components/InvoiceModal.js
+++ src/components/InvoiceModal.js
@@ -18,7 +18,7 @@ function GenerateInvoice() {
       format: [612, 792]
     });
     pdf.internal.scaleFactor = 1;
-    const imgProps= pdf.getImageProperties(imgData);
+    const imgProps = pdf.getImageProperties(imgData);
     const pdfWidth = pdf.internal.pageSize.getWidth();
     const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
     pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
@@ -27,9 +27,6 @@ function GenerateInvoice() {
 }
 
 class InvoiceModal extends React.Component {
-  constructor(props) {
-    super(props);
-  }
   render() {
     return(
       <div>
@@ -37,33 +34,33 @@ class InvoiceModal extends React.Component {
           <div id="invoiceCapture">
             <div className="d-flex flex-row justify-content-between align-items-start bg-light w-100 p-4">
               <div className="w-100">
-                <h4 className="fw-bold my-2">{this.props.info.billFrom||'John Uberbacher'}</h4>
+                <h4 className="fw-bold my-2">{this.props.info.billFrom || 'John Uberbacher'}</h4>
                 <h6 className="fw-bold text-secondary mb-1">
-                  Invoice #: {this.props.info.invoiceNumber||''}
+                  Invoice #: {this.props.info.invoiceNumber || ''}
                 </h6>
               </div>
               <div className="text-end ms-4">
                 <h6 className="fw-bold mt-1 mb-2">Amount&nbsp;Due:</h6>
-                <h5 className="fw-bold text-secondary"> {this.props.currency} {this.props.total}</h5>
+                <h5 className="fw-bold text-secondary">{this.props.currency} {this.props.total}</h5>
               </div>
             </div>
             <div className="p-4">
               <Row className="mb-4">
                 <Col md={4}>
                   <div className="fw-bold">Billed to:</div>
-                  <div>{this.props.info.billTo||''}</div>
-                  <div>{this.props.info.billToAddress||''}</div>
-                  <div>{this.props.info.billToEmail||''}</div>
+                  <div>{this.props.info.billTo || ''}</div>
+                  <div>{this.props.info.billToAddress || ''}</div>
+                  <div>{this.props.info.billToEmail || ''}</div>
                 </Col>
                 <Col md={4}>
                   <div className="fw-bold">Billed From:</div>
-                  <div>{this.props.info.billFrom||''}</div>
-                  <div>{this.props.info.billFromAddress||''}</div>
-                  <div>{this.props.info.billFromEmail||''}</div>
+                  <div>{this.props.info.billFrom || ''}</div>
+                  <div>{this.props.info.billFromAddress || ''}</div>
+                  <div>{this.props.info.billFromEmail || ''}</div>
                 </Col>
                 <Col md={4}>
                   <div className="fw-bold mt-2">Date Of Issue:</div>
-                  <div>{this.props.info.dateOfIssue||''}</div>
+                  <div>{this.props.info.dateOfIssue || ''}</div>
                 </Col>
               </Row>
               <Table className="mb-0">
@@ -76,20 +73,14 @@ class InvoiceModal extends React.Component {
                   </tr>
                 </thead>
                 <tbody>
-                  {this.props.items.map((item, i) => {
-                    return (
-                      <tr id={i} key={i}>
-                        <td style={{width: '70px'}}>
-                          {item.quantity}
-                        </td>
-                        <td>
-                          {item.name} - {item.description}
-                        </td>
-                        <td className="text-end" style={{width: '100px'}}>{this.props.currency} {item.price}</td>
-                        <td className="text-end" style={{width: '100px'}}>{this.props.currency} {item.price * item.quantity}</td>
-                      </tr>
-                    );
-                  })}
+                  {this.props.items.map((item, i) => (
+                    <tr id={i} key={i}>
+                      <td style={{width: '70px'}}>{item.quantity}</td>
+                      <td>{item.name} - {item.description}</td>
+                      <td className="text-end" style={{width: '100px'}}>{this.props.currency} {item.price}</td>
+                      <td className="text-end" style={{width: '100px'}}>{this.props.currency} {item.price * item.quantity}</td>
+                    </tr>
+                  ))}
                 </tbody>
               </Table>
               <Table>
@@ -104,14 +95,14 @@ class InvoiceModal extends React.Component {
                     <td className="fw-bold" style={{width: '100px'}}>SUBTOTAL</td>
                     <td className="text-end" style={{width: '100px'}}>{this.props.currency} {this.props.subTotal}</td>
                   </tr>
-                  {this.props.taxAmmount != 0.00 &&
+                  {this.props.taxAmmount !== 0.00 &&
                     <tr className="text-end">
                       <td></td>
                       <td className="fw-bold" style={{width: '100px'}}>TAX</td>
                       <td className="text-end" style={{width: '100px'}}>{this.props.currency} {this.props.taxAmmount}</td>
                     </tr>
                   }
-                  {this.props.discountAmmount != 0.00 &&
+                  {this.props.discountAmmount !== 0.00 &&
                     <tr className="text-end">
                       <td></td>
                       <td className="fw-bold" style={{width: '100px'}}>DISCOUNT</td>
